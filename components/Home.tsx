1182116513c5a8335a65b2c07c107cf4bd765b20
Stanlisberg <nnabugo2010@gmail.com>
2023-12-10 21:24:09 +0100

    first-commit

---
+++ components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-5xl border border-black h-screen'>This is the Home page
+    <nav>This is the nav</nav>
+    </div>
+  )
+}
+
+export default Home
